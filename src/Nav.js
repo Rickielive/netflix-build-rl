a6b57fd5dc3ff53026de148a0f5a263dac615c26
Rickielive <wachmureithi@gmail.com>
2021-07-31 12:01:43 +0300

    init

---
+++ src/Nav.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import './Nav.css';
+
+function Nav() {
+    return (
+        <div className= "nav">
+            div.nav__contents
+            <img 
+            className= "nav__logo"
+            src= "https://images.unsplash.com/photo-1611162617474-5b21e879e113?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bmV0ZmxpeCUyMGxvZ298ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80" 
+            alt=""
+            />
+
+
+            <img 
+            className = "nav__avatar"
+            src= "http://www.sjuhawknews.com/wp-content/uploads/2020/06/A70106FB-AB28-4911-BCE4-5CE7F10C717D-810x526.png"
+             alt=""
+             /> 
+
+            <h1> This is the Wachira</h1>
+        </div>
+    )
+}
+
+export default Nav
