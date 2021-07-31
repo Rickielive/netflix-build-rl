66a6e61e52b2fe14b282e1ed21b722949c4491e7
Rickielive <wachmureithi@gmail.com>
2021-07-31 12:02:41 +0300

    init

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCDrvxI8n4NsuyZFdDjwSJmogfY84a1G8M",
+    authDomain: "netflix-build-rl.firebaseapp.com",
+    projectId: "netflix-build-rl",
+    storageBucket: "netflix-build-rl.appspot.com",
+    messagingSenderId: "952205189032",
+    appId: "1:952205189032:web:79e55b708146fe602b479d",
+    measurementId: "G-MTP9PQTXG0"
+ };
