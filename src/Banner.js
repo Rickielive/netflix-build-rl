5172bc8b66438afbea11fa4493f4209439d7f70f
Rickielive <wachmureithi@gmail.com>
2021-08-06 13:06:29 +0300

    commit 3

--- src/Banner.js
+++ src/Banner.js
@@ -1,5 +1,6 @@
+import React from 'react'
 import './Banner.css';
-// import axios from "/axios";
+import axios from "./axios";
 import requests from "./Requests";
 import { useState, useEffect } from 'react';
 
@@ -8,27 +9,27 @@ import { useState, useEffect } from 'react';
 function Banner() {
     const [movie,setMovie] = useState([]);
 
-    // useEffect(() => {
-    //     async function fetchData() {
-    //         const request = await axios.get(request.fetchNetflixOriginals);
-    //         setMovie(
-    //             request.data.results[
-    //                 Math.floor(Math.random()* request.data.results.length -1)
-    //             ]
-    //         );
+    useEffect(() => {
+        async function fetchData() {
+            const request = await axios.get(requests.fetchNetflixOriginals);
+            setMovie(
+                request.data.results[
+                    Math.floor(Math.random()* request.data.results.length -1)
+                ]
+            );
             
-    //         return request;
-    //     }
-    //     fetchData();
+            return request;
+        }
+        fetchData();
 
-    // }, []);
+    }, []);
 
-    // console.log(movie);
+    console.log(movie);
 
 
 
     function truncate (string , n){
-        return string?.length > n? string.substr(0 , n-1)+ '...' : string;
+        return string && string.length > n ? string.substr(0, n - 1) + "..." : string;
     }
 
 
@@ -46,7 +47,7 @@ function Banner() {
                     <button className='banner__button'>Play</button>
                     <button className='banner__button'>My list</button>
                 </div>
-                <h1 className= 'banner__description'>{truncate (`This is a test description`, 20)}</h1>
+                <h1 className= 'banner__description'>{truncate (`This is a test description`, 10)}</h1>
             </div>
             <div className="banner__fadeBottom"/>  
             {/* Gives that nice fade at the end */}
