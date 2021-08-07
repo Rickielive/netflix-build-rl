96c8348fb5eb693bfa25fbe2f05fde49b26e7926
Rickielive <wachmureithi@gmail.com>
2021-08-07 13:09:31 +0300

    9th commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
 import React from 'react';
 import './App.css';
-import HomeScreen from './HomeScreen.js';
-import './HomeScreen.css';
+import HomeScreen from './Screens/HomeScreen.js';
+
+// using ES6 modules
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import LoginScreen from './Screens/LoginScreen';
 
 function App() {
+const user = null;
+
   return (
     <div className="app">
-      <HomeScreen />
+
+      <Router>
+        {!user ? (
+          <LoginScreen />
+        ): (
+          <Switch>
+          <Router exact path="/">
+              <HomeScreen />
+            </Router>
+        </Switch>
+        )}
+        
+      </Router>
     </div>
   );
 }
 
 export default App;
+
+
+
+
+{/* <Route path="/about">
+            <About/>
+            </Route>
+            <Router path="/users">
+              <Users/>
+            </Router> */}
---
+++ src/Screens/LoginScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Loginscreen.css';
+
+function LoginScreen() {
+    return (
+        <div className = 'loginScreen'>
+            
+        </div>
+    )
+}
+
+export default LoginScreen
